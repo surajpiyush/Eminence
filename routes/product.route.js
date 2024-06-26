9ac4906dc0974d843b2ec64bd7999d05855b0321
surajpiyush <piyushkumar9648@gmail.com>
2024-06-26 09:43:33 +0530

    updated env and authorization

--- routes/product.route.js
+++ routes/product.route.js
@@ -6,7 +6,7 @@ const productRouter=express.Router()
 
 productRouter.post('/createProduct',auth,authorize(['admin']),upload,createProduct)
 productRouter.put('/updateProduct/:productId',auth,authorize(['admin']),upload,updateProduct)
-productRouter.get('/allProducts',auth,authorize(['admin']),getProducts)
+productRouter.get('/allProducts',auth,getProducts)
 
 
 module.exports=productRouter
